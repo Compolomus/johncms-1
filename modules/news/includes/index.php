c30ec1fcffe3ce54f56243b1d1ee64fc81dcc78a
Compolomus <compolom@gmail.com>
2019-12-25 06:19:13 +0200

    test

--- modules/news/includes/index.php
+++ modules/news/includes/index.php
@@ -22,10 +22,54 @@ defined('_IN_JOHNCMS') || die('Error: restricted access');
 $total = $db->query('SELECT COUNT(*) FROM `news`')->fetchColumn();
 $req = $db->query("SELECT * FROM `news` ORDER BY `time` DESC LIMIT ${start}, " . $user->config->kmess);
 
+/* pagination test  */
+
+function displayPagination(string $url, int $start, int $total, int $kmess): string
+{
+    $nav = new \Compolomus\Pagination\Pagination(($start / $kmess + 1), $kmess, $total, 2); // 2 как в ждоне, удобнее использовать $page так как старт вычисляется пагинацией
+
+    #echo '<pre>' . print_r($nav->get(), true) . '</pre>';
+
+    $out[] = '<ul class="pagination">' . PHP_EOL . "\t";
+
+    foreach ($nav->get() as $type => $page) {
+        #echo $type . ' - ' . $page . PHP_EOL;
+        switch ($type) {
+            case 'minus':
+                $out[] = '<li class="page-item"><a class="page-link" href="?page=' . $page . '">&lt;&lt;</a></li>';
+                break;
+//            case 'first':
+//                $out[] = '<li class="page-item"><a class="page-link" href="?page=' . $page . '">1</a></li>';
+//                break;
+            case 'plus':
+                $out[] = '<li class="page-item"><a class="page-link" href="?page=' . $page . '">&gt;&gt;</a></li>';
+                break;
+//            case 'last':
+//                $out[] = '<li class="page-item"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
+//                break;
+            case 'current':
+                $out[] = '<li class="page-item active"><a class="page-link" href="#">' . $page . '</a></li>';
+                break;
+            case 'rightDots':
+            case 'leftDots':
+                $out[] = '<li class="page-item disabled"><a class="page-link" href="#">' . $page . '</a></li>';
+                break;
+            default:
+                #echo $type . ' - ' . $page . PHP_EOL;
+                $out[] = '<li class="page-item"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
+                break;
+        }
+    }
+
+    $out[] = '</ul>' . PHP_EOL;
+
+    return implode(PHP_EOL . "\t", $out);
+}
+
 echo $view->render(
     'news::index',
     [
-        'pagination' => $tools->displayPagination('?', $start, $total, $user->config->kmess),
+        'pagination' => displayPagination('?', $start, $total, $user->config->kmess),
         'total'      => $total,
         'list'       =>
             function () use ($req, $tools, $db) {
